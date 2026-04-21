e27fe9386c320ee457884a56f98d4f23470499ba
Uppari Praveen <upparipraveen2003@gmail.com>
2026-04-21 07:06:18 +0530

    created generators and iterators

---
+++ generators and iterator in python.py
@@ -0,0 +1,22 @@
+#taking number
+# import random;
+
+# ls =[]
+# for i in range(5):
+#     ls.append(random.randint(1,10))
+
+
+# def myGeneerator():
+#     print("first");
+#     yield 1;
+#     print("second");
+#     yield 2;
+#     print("three");
+#     yield 3;
+#     print("four")
+
+
+# res = myGeneerator();
+# print(next(res))
+# print(next(res))
+# print(next(res))
