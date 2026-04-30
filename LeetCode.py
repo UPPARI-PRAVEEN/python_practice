4ea5ced992afdde20ea82d173cd7cf33c9efae01
Uppari Praveen <upparipraveen2003@gmail.com>
2026-04-30 07:45:06 +0530

    code optimation

--- LeetCode.py
+++ LeetCode.py
@@ -1,433 +1,461 @@
-# ls = [1, 2, 3, 4, 5, 6];#[1, 3, 5, 2, 4, 6]
-# #mmove even to end
+ls = [1, 2, 3, 4, 5, 6];#[1, 3, 5, 2, 4, 6]
+#mmove even to end
 
-# for i in range(len(ls)):
-#     for j in range(len(ls)-1):
-#         if ls[j] % 2 == 0:
-#             temp = ls[j]
-#             ls[j] = ls[j+1]
-#             ls[j+1] = temp
-# print(ls)
+ls = [1, 2, 3, 4, 5, 6]
 
-#reverse list
-# ls = [1, 2, 3, 4, 5, 6]
+left = 0
+right = len(ls) - 1
 
-# start = 0
-# end = len(ls)-1
-# while start < end:
-#     temp = ls[start]
-#     ls[start] = ls[end]
-#     ls[end] = temp
-#     start +=1
-#     end -=1
-# print(ls)
+while left < right:
+    if ls[left] % 2 != 0:
+        left += 1
+    else:
+        ls[left], ls[right] = ls[right], ls[left]
+        right -= 1
 
-# ls =[1,2,3,5]
-# #find missing number in list
-# for i in range(len(ls)-1):
-#     if ls[i]+1 != ls[i+1]:
-#         print(ls[i]+1)
+print(ls)
 
-# st = "programming"
-# #firt occurence of non repeating character
-# for i in range(len(st)):
-#     if st[i] not in st[i+1:]:
-#         print(st[i])
-#         break
+for i in range(len(ls)):
+    for j in range(len(ls)-1):
+        if ls[j] % 2 == 0:
+            temp = ls[j]
+            ls[j] = ls[j+1]
+            ls[j+1] = temp
+print(ls)
 
-#exception handling
-# try:
-#     num = int(input("Enter a number: "))
-#     result = 10 / num
-#     print("Result:", result)
+reverse list
+ls = [1, 2, 3, 4, 5, 6]
 
-# except ValueError:
-#     print("Invalid input! Please enter a valid integer.")
+start = 0
+end = len(ls)-1
+while start < end:
+    temp = ls[start]
+    ls[start] = ls[end]
+    ls[end] = temp
+    start +=1
+    end -=1
+print(ls)
 
-# except ZeroDivisionError:
-#     print("Cannot divide by zero.")
+ls =[1,2,3,5]
+#find missing number in list
+for i in range(len(ls)-1):
+    if ls[i]+1 != ls[i+1]:
+        print(ls[i]+1)
+to find missing number in list with 0 to n
+ls = [0,1,2,4,5]
+total_sum = len(ls) * (len(ls) + 1) // 2
+actual_sum = sum(ls)
+if total_sum - actual_sum != 0:
+    print(total_sum - actual_sum)
+else:    
+    print("no missing number")
 
-# except Exception as e:
-#     print("Something went wrong:", e)
+ls = [1, 6]
 
-# finally:
-#     print("Program executed successfully.")
+for i in range(len(ls)-1):
+    if ls[i]+1 != ls[i+1]:
+        for j in range(ls[i]+1,ls[i+1]):
+             print(j)
+
+
+st = "programming"
+#firt occurence of non repeating character
+for i in range(len(st)):
+    if st[i] not in st[i+1:]:
+        print(st[i])
+        break
+
+exception handling
+try:
+    num = int(input("Enter a number: "))
+    result = 10 / num
+    print("Result:", result)
+
+except ValueError:
+    print("Invalid input! Please enter a valid integer.")
+
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
+
+except Exception as e:
+    print("Something went wrong:", e)
+
+finally:
+    print("Program executed successfully.")
         
-# transactions = [
-#     {"user": "A", "amount": 100},
-#     {"user": "B", "amount": 200},
-#     {"user": "A", "amount": 300},
-#     {"user": "C", "amount": 400},
-#     {"user": "B", "amount": 100}
-# ]#{'A': 400, 'B': 300, 'C': 400}
-# dic = {}
-# for i in range(len(transactions)):
+transactions = [
+    {"user": "A", "amount": 100},
+    {"user": "B", "amount": 200},
+    {"user": "A", "amount": 300},
+    {"user": "C", "amount": 400},
+    {"user": "B", "amount": 100}
+]#{'A': 400, 'B': 300, 'C': 400}
+dic = {}
+for i in range(len(transactions)):
    
-#     if transactions[i]["user"] in dic:
-#         print("hi")
-#         dic[transactions[i]["user"] ] += transactions[i]["amount"]
-#     else: 
-#         print("first")
-#         dic[transactions[i]["user"] ] = transactions[i]["amount"]
+    if transactions[i]["user"] in dic:
+        print("hi")
+        dic[transactions[i]["user"] ] += transactions[i]["amount"]
+    else: 
+        print("first")
+        dic[transactions[i]["user"] ] = transactions[i]["amount"]
        
-# print(dic)
+print(dic)
     
-# ls = [1,2,3,4,5,6];
-# k = 3;
-# #[4, 5, 6, 1, 2, 3]
+ls = [1,2,3,4,5,6];
+k = 3;
+#[4, 5, 6, 1, 2, 3]
 
-# def reverse(ls,st,ed):
+def reverse(ls,st,ed):
     
-#     while st < ed:
-#         temp = ls[st]
-#         ls[st] = ls[ed]
-#         ls[ed] = temp
-#         st += 1;
-#         ed -= 1;
-#     return ls
+    while st < ed:
+        temp = ls[st]
+        ls[st] = ls[ed]
+        ls[ed] = temp
+        st += 1;
+        ed -= 1;
+    return ls
 
-# reverse(ls,0,len(ls)-1)
-# reverse(ls,0,k-1)
-# reverse(ls,k,len(ls)-1)
+reverse(ls,0,len(ls)-1)
+reverse(ls,0,k-1)
+reverse(ls,k,len(ls)-1)
 
 
-# print(ls)
+print(ls)
 
-# ls =  [-2,1,-3,4,-1,2,1,-5,4]
-# currSum = ls[0] #1 -2 4 3 5
-# maxSum =ls[0]
-# stIndex = 0
-# endIndex = 0
-# tempIndex = 0;
-# #Max subarray sum 
-# #OUTUT: 6 [4,-1,2,1]
-# for i in range(1,len(ls)):
-#     if ls[i] > currSum+ls[i]:
-#         currSum = ls[i]
-#         tempIndex = i//1
-#     else:
-#         currSum += ls[i]
-#     if currSum > maxSum:# 1 > -2
-#         maxSum = currSum #1
-#         stIndex = tempIndex #1 4
-#         endIndex = i;#1 5 6
-# print(stIndex,endIndex)
-# print(ls[stIndex:endIndex+1])
+ls =  [-2,1,-3,4,-1,2,1,-5,4]
 
-# st = "I love python"
-# # Output: "python love I"
-# res = "";
-# word = ""
-# for ch in st:
+max_sum = float('-inf')
+curr_sum = 0;
+tempStart = 0
+start = 0
+end = 0;
+
+for i in range(len(ls)):
+    if curr_sum == 0:
+        tempStart = i
+    curr_sum += ls[i]
+    if curr_sum > max_sum:
+        max_sum = curr_sum
+        start = tempStart
+        end = i
+    if curr_sum < 0:
+        curr_sum = 0
+print(ls[start:end+1])
+
+st = "I love python"
+# Output: "python love I"
+res = "";
+word = ""
+for ch in st:
     
-#     if ch != ' ':
-#         word += ch;
-#     else: 
-#         res = word + " " + res;
-#         word = ""
-# res = word + " " + res
+    if ch != ' ':
+        word += ch;
+    else: 
+        res = word + " " + res;
+        word = ""
+res = word + " " + res
 
-# print(res)
+print(res)
 
 
-# ls = [1,3,2,4]
-# targetSum = 6
-# res = []
+ls = [1,3,2,4]
+targetSum = 6
+res = []
 
-# for i in range(len(ls)):
-#     nested = []
-#     sumEle = 0
+for i in range(len(ls)):
+    nested = []
+    sumEle = 0
     
-#     for j in range(i, len(ls)):
-#         nested.append(ls[j])
-#         sumEle += ls[j]
+    for j in range(i, len(ls)):
+        nested.append(ls[j])
+        sumEle += ls[j]
         
-#         if sumEle == targetSum:
-#             res.append(nested[:])   # copy because nested will change in next iteration what happens if we append nested directly 
-                                        # nested will be modified in next iteration and res will have reference to nested so it will change as well
-#             break
+        if sumEle == targetSum:
+            res.append(nested[:])   # copy because nested will change in next iteration what happens if we append nested directly 
+                                        nested will be modified in next iteration and res will have reference to nested so it will change as well
+            break
 
-# print(res)#[[1, 3, 2], [2, 4]]
-
-
-
-# st = "xyz" #azy
-# res = ""
-# for ch in st:
-#     chars = ""
-#     if ord(ch) == 122 or ord(ch) == 90:
-#         chars = chr(ord(ch) - 25)
-#     else:
-#         chars = chr(ord(ch) + 1)
-#     res = chars + res
-#     chars = ""
-# print(res)
+print(res)#[[1, 3, 2], [2, 4]]
 
 
-# 
 
-#cecking amstrong number or not
-# num = 153;
-# temp = num;
-# def countNum(val):
-#     para = val
-#     count = 0;
-#     print(para)
-#     while para !=0:
-#         count += 1;
-#         para = para // 10;
-#     return count;
-# def isMul(num, count):
-#     print(num ** count,"ismul")
-#     return num ** count  
-# def result(count,num):
+st = "xyz" #azy
+res = ""
+for ch in st:
+    chars = ""
+    if ord(ch) == 122 or ord(ch) == 90:
+        chars = chr(ord(ch) - 25)
+    else:
+        chars = chr(ord(ch) + 1)
+    res = chars + res
+    chars = ""
+print(res)
+
+
+
+
+# cecking amstrong number or not
+num = 153;
+temp = num;
+def countNum(val):
+    para = val
+    count = 0;
+    print(para)
+    while para !=0:
+        count += 1;
+        para = para // 10;
+    return count;
+def isMul(num, count):
+    return num ** count  
+def result(count,num):
     
-#     sumNum = 0;
-#     for i in range(0,count):
-#         print(num,"number digit")
-#         sumNum += isMul(num % 10,count)
-#         num = num // 10;
-#     return sumNum;
+    sumNum = 0;
+    for i in range(0,count):
+        print(num,"number digit")
+        sumNum += isMul(num % 10,count)
+        num = num // 10;
+    return sumNum;
         
-# count = countNum(num)
-# res = result(count,num)
-# print(res)
-# if res == temp:
-#     print("is amstromstorn number");
-# else:
-#     print("not amstrong number")
+count = countNum(num)
+res = result(count,num)
+print(res)
+if res == temp:
+    print("is amstromstorn number");
+else:
+    print("not amstrong number")
 
 
         
-#    sypnum     
-#     num = 1124;
+   sypnum     
+    num = 1124;
 
-# def sumOfdigit(num, isAdd):
-#     result = 0 if isAdd else 1   # ✔ Python ternary
+def sumOfdigit(num, isAdd):
+    result = 0 if isAdd else 1   # ✔ Python ternary
 
-#     val = num
-#     while val != 0:
-#         digit = val % 10
+    val = num
+    while val != 0:
+        digit = val % 10
 
-#         if isAdd:
-#             result += digit
-#         else:
-#             result *= digit
+        if isAdd:
+            result += digit
+        else:
+            result *= digit
 
-#         val = val // 10
+        val = val // 10
 
-#     return result
-# sumNum = sumOfdigit(num,True)
-# mulNum = sumOfdigit(num,False)
-# print(sumNum,mulNum)
-
-
-#factorial of number
-# num = 5;
-
-# def fact(num):
-#     if num == 0:
-#         return 1
-#     return num * fact(num - 1)
-# res = fact(num)
-# print(res)
-
-# num = 4;
-# res = 1;
-# for i in range(1,num+1):
-#     res = res * i;
-# print(res)
-
-# prime = 11
-# isPrime = True
-
-# for i in range(2,5):
-#     if prime % i == 0:
-#         isPrime =False
-#         break
-# if isPrime :
-#     print("is a prime number")
-# else:
-#     print("not a primenumber")
-
-#print prime numbers in range or 1 to n
-# n = 20;   
-# for i in range(2,n+1):
-#     isPrime = True    
-#     for j in range(2,i//2+1):
-#         if i % j == 0:    
-#             isPrime = False
-#             break 
-#     if isPrime:
-#         print(i)  
+    return result
+sumNum = sumOfdigit(num,True)
+mulNum = sumOfdigit(num,False)
+print(sumNum,mulNum)
 
 
-# ls = [1,0,2,0,3,0,4]
-# #move zero to end
-# pos = 0
-# for i in range(len(ls)):
-#     if ls[i] != 0:
-#         ls[pos], ls[i] = ls[i], ls[pos]
-#         pos += 1
+factorial of number
+num = 5;
 
-# print(ls)
+def fact(num):
+    if num == 0:
+        return 1
+    return num * fact(num - 1)
+res = fact(num)
+print(res)
 
-# string = "aabbcde"
-# # Output: c fist occurance not repeating character
-# revString = ""
-# for i in range(len(string)-1):
-#     if string[i] not in string[i+1:] and string[i] not in revString:
-#         print(string[i]);
-#         break;
-#     else:
-#         revString += string[i]
+num = 4;
+res = 1;
+for i in range(1,num+1):
+    res = res * i;
+print(res)
 
-# string = "aabbcde"
+prime = 11
+isPrime = True
 
-# freq = {}
+for i in range(2,5):
+    if prime % i == 0:
+        isPrime =False
+        break
+if isPrime :
+    print("is a prime number")
+else:
+    print("not a primenumber")
 
-# for ch in string:
-#     freq[ch] = freq.get(ch, 0) + 1
+print prime numbers in range or 1 to n
+n = 20;   
+for i in range(2,n+1):
+    isPrime = True    
+    for j in range(2,i//2+1):
+        if i % j == 0:    
+            isPrime = False
+            break 
+    if isPrime:
+        print(i)  
 
-# for ch in string:
-#     if freq[ch] == 1:
-#         print(ch)
-#         break
+
+ls = [1,0,2,0,3,0,4]
+#move zero to end
+pos = 0
+for i in range(len(ls)):
+    if ls[i] != 0:
+        ls[pos], ls[i] = ls[i], ls[pos]
+        pos += 1
+
+print(ls)
+
+string = "aabbcde"
+# Output: c fist occurance not repeating character
+revString = ""
+for i in range(len(string)-1):
+    if string[i] not in string[i+1:] and string[i] not in revString:
+        print(string[i]);
+        break;
+    else:
+        revString += string[i]
+
+string = "aabbcde"
+
+freq = {}
+
+for ch in string:
+    freq[ch] = freq.get(ch, 0) + 1
+
+for ch in string:
+    if freq[ch] == 1:
+        print(ch)
+        break
 
 
-# arr = [10, 5, 8, 20, 20]
+arr = [10, 5, 8, 20, 20]
 
-# first = float('-inf')
-# second = float('-inf')
+first = float('-inf')
+second = float('-inf')
 
-# for num in arr:
-#     if num > first:
-#         second = first
-#         first = num
-#     elif first > num > second:
-#         second = num
+for num in arr:
+    if num > first:
+        second = first
+        first = num
+    elif first > num > second:
+        second = num
 
-# print(second)
+print(second)
 
-# s = "abcabcbb"
-# # Output: True
+s = "abcabcbb"
+# Output: "abc"
 
-# def longesSub(string):
-#     logGeststr = "";
-#     for i in range(len(string)):
-#         subStr = ""
-#         for j in range(i,len(string)-1):
-#             if string[j] in subStr:
-#                 break;
-#             else:
-#                 subStr += string[j];
-#         if len(subStr) > len(logGeststr):
-#             logGeststr = subStr
+def longesSub(string):
+    logGeststr = "";
+    for i in range(len(string)):
+        subStr = ""
+        for j in range(i,len(string)-1):
+            if string[j] in subStr:
+                break;
+            else:
+                subStr += string[j];
+        if len(subStr) > len(logGeststr):
+            logGeststr = subStr
             
-#     return logGeststr;
-# sorting1 = longesSub(s)
-# print(sorting1)
+    return logGeststr;
+sorting1 = longesSub(s)
+print(sorting1)
 
 
-# s = "aaabbcc"
-# # Output: "a3b2c1"
-# res = "";
-# count = 0;
+s = "aaabbcc"
+# Output: "a3b2c1"
+res = "";
+count = 0;
 
 
-# for i in range(len(s)-1):
-#     ch = s[i];
-#     if ch == s[i+1]:
-#         count += 1;
-#         print(ch,count)
-#     else:
-#         res = res + ch + str(count+1)
+for i in range(len(s)-1):
+    ch = s[i];
+    if ch == s[i+1]:
+        count += 1;
+        print(ch,count)
+    else:
+        res = res + ch + str(count+1)
         
-#         count = 0
-# res = res + s[-1] + str(count + 1)
-# print(res)
+        count = 0
+res = res + s[-1] + str(count + 1)
+print(res)
 
-# ls =  [16,17,4,3,5,2]
-# # Output: [17,5,2]
-# res =[];
-# for i in range(len(ls)-1):
-#     if ls[i] < ls[i+1]:
-#         res.append(ls[i+1])
-# res.append(ls[-1])       
-# print(res)
+ls =  [16,17,4,3,5,2]
+# Output: [17,5,2]
+res =[];
+for i in range(len(ls)-1):
+    if ls[i] < ls[i+1]:
+        res.append(ls[i+1])
+res.append(ls[-1])       
+print(res)
 
-# nums = [2,7,11,15]
-# target = 9
+nums = [2,7,11,15]
+target = 9
 
-# mp = {}
+mp = {}
 
-# for i in range(len(nums)):
-#     diff = target - nums[i]
+for i in range(len(nums)):
+    diff = target - nums[i]
     
-#     if diff in mp:
-#         print([mp[diff], i])
-#         break
+    if diff in mp:
+        print([mp[diff], i])
+        break
     
-#     mp[nums[i]] = i
+    mp[nums[i]] = i
 
-# s = "madams"
-# ls = list(s);
-# start = 0;
-# end = len(ls)-1
-# while start < end:
+s = "madams"
+ls = list(s);
+start = 0;
+end = len(ls)-1
+while start < end:
    
     
-#     ls[start],ls[end] = ls[end],ls[start]
+    ls[start],ls[end] = ls[end],ls[start]
         
-#     start+=1;
-#     end-=1;
-# print("".join(ls))
+    start+=1;
+    end-=1;
+print("".join(ls))
 
-# s = "madam"
+s = "madam"
 
-# start = 0
-# end = len(s) - 1
+start = 0
+end = len(s) - 1
 
-# isPalindrome = True
+isPalindrome = True
 
-# while start < end:
-#     if s[start] != s[end]:
-#         isPalindrome = False
-#         break
-#     start += 1
-#     end -= 1
+while start < end:
+    if s[start] != s[end]:
+        isPalindrome = False
+        break
+    start += 1
+    end -= 1
 
-# print(isPalindrome)
+print(isPalindrome)
 
 
-# ls = [1,-2,3,-4,5]
-# # Output: [-2,-4,1,3,5]  (order not important)
+ls = [1,-2,3,-4,5]
+# Output: [-2,-4,1,3,5]  (order not important)
 
-# start =0
-# end = len(ls)-2
+start =0
+end = len(ls)-2
 
-# while start < end:
-#     if ls[start] > 0:
-#         ls[start],ls[end] = ls[end],ls[start]
-#     start +=1
-#     end -=1
+while start < end:
+    if ls[start] > 0:
+        ls[start],ls[end] = ls[end],ls[start]
+    start +=1
+    end -=1
     
-# print(ls)
+print(ls)
 
-#++++++++++++++++++++++++++++++++++++++++++++ + sliding window ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#**************** find max sum of subarray of size k ****************
-# arr = [2,1,5,1,3,2]
-# k = 3
+++++++++++++++++++++++++++++++++++++++++++++ + sliding window ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+**************** find max sum of subarray of size k ****************
+arr = [2,1,5,1,3,2]
+k = 3
 
-# # Output: 9   # (5+1+3)
-# window_sum = sum(arr[:k])
-# max_sum = window_sum
+# Output: 9   # (5+1+3)
+window_sum = sum(arr[:k])
+max_sum = window_sum
 
-# for i in range(k,len(arr)):
-#     window_sum += arr[i]
-#     window_sum -= arr[i-k]
-#     max_sum = max(max_sum,window_sum)
-# print(max_sum)
+for i in range(k,len(arr)):
+    window_sum += arr[i]
+    window_sum -= arr[i-k]
+    max_sum = max(max_sum,window_sum)
+print(max_sum)
 
 #********** Revemove duplicates from sorted array **********
 arr = [1, 1, 2, 2, 3, 3]
@@ -561,10 +589,14 @@ class Solution(object):
 
         dp[index][target] = take + not_take
         return dp[index][target]
+        #input: amount = 4, coins = [1, 2, 3]
+        #output: 4 (there are four ways to make up the amount: 1) 1+1+1+1, 2) 1+1+2, 3) 1+3, 4) 2+2)
 
 
 #****** 0/1 knap scact with paint walls ******************
 class Solution(object):
+    #input: cost = [1, 2, 3], time = [1, 2, 3]
+    #output: 2 (paint wall 0 with cost 1, then skip wall 1 because painter is busy, and paint wall 2 with cost 3. Total cost = 1 + 3 = 4)
     def paintWalls(self, cost, time):
         n = len(cost)
         dp = {}
@@ -619,6 +651,8 @@ class Solution(object):
         :type text2: str
         :rtype: int
         """
+        #input: text1 = "abcde", text2 = "ace"
+        #output: 3 (longest common subsequence is "ace" and its length is 3.)
         len1 = len(text1)
         len2 = len(text2)
         dp = [[-1 for _ in range(len2)] for _ in range(len1)]
--- paterns.py
+++ paterns.py
@@ -48,21 +48,21 @@
 #   *
 
 
-# n = 4
+n = 4
 
-# for i in range(1,n+1):
-#     for j in range(n - i):
-#         print(" ",end = "")
-#     for j in range(i):
-#         print("*",end = " ")
-#     print("")
+for i in range(1,n+1):
+    for j in range(n - i):
+        print(" ",end = "")
+    for j in range(i):
+        print("*",end = " ")
+    print("")
 
-# for i in range(n-1,0,-1):
-#     for j in range(n - i):
-#         print(" ",end = "")
-#     for j in range(i):
-#         print("*",end = " ")
-#     print("")
+for i in range(n-1,0,-1):
+    for j in range(n - i):
+        print(" ",end = "")
+    for j in range(i):
+        print("*",end = " ")
+    print("")
 
 
 # A
