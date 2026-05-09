4dbaa96b5f0b25eaffc2c7157f28cc0fddd37092
Uppari Praveen <upparipraveen2003@gmail.com>
2026-05-09 18:48:04 +0530

    longest consective stirng

--- LeetCode.py
+++ LeetCode.py
@@ -1465,6 +1465,29 @@ class Solution(object):
                 longest = max(longest,length)
         return longest
 
+#********* Longest Consecutive Sequence 128 ******************
+#input: nums = [100,4,200,1,3,2]
+#output: 4 (the longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.)
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        seen = set(nums)
+        longest = 0
+        for num in seen:
+            
+            if num - 1 not in seen:
+                length = 1
+                current = num
+                while current +1 in seen:
+                    current +=1
+                    length +=1
+                longest = max(longest,length)
+        return longest
+
+
         
         
 
