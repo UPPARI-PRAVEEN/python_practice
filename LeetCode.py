9979590280f7ad10f2ec8a35dc0e33e0b1d786fa
Uppari Praveen <upparipraveen2003@gmail.com>
2026-06-17 15:06:25 +0530

    ispalin version 2

--- LeetCode.py
+++ LeetCode.py
@@ -658,6 +658,26 @@ for i in range(len(s)):
         st += s[i]
 print(isPalin(0,len(st)-1,st))
 
+or
+def isPalindrome(s):
+    l, r = 0, len(s) - 1
+
+    while l < r:
+        while l < r and not s[l].isalnum():
+            l += 1
+        while l < r and not s[r].isalnum():
+            r -= 1
+
+        if s[l].lower() != s[r].lower():
+            return False
+
+        l += 1
+        r -= 1
+
+    return True
+
+print(isPalindrome("A man, a plan, a canal: Panama"))
+
 
 #### remove adjacent duplicates leetcode 1047
 # Input: s = "abbaca"
